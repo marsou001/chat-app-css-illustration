121e0dcf9872a255cd7dc318b34fc0aac2add71f
Marouane Souda <marsou431@gmail.com>
2021-03-12 12:50:29 +0100

    fix: add key to React.Fragment in Messages.tsx

--- src/components/Phone/PhoneMessages/Messages/Messages.tsx
+++ src/components/Phone/PhoneMessages/Messages/Messages.tsx
@@ -2,6 +2,7 @@ import Message from './Message/Message';
 import Images from './Images/Images';
 import MessagesContainer from './MessagesContainer/MessagesContainer';
 import MessagesReceivedRates from './MessagesReceivedRates/MessagesReceivedRates';
+import React from 'react';
 
 interface MessageArr {
     id: number,
@@ -21,11 +22,11 @@ function Messages({ sent, images, rate, messages }: MessagesProps) {
             <Images sent={sent} images={images} />
             <MessagesContainer sent={sent}>                
                 {messages.map((message) => (
-                    <>
-                        <Message key={message.id} sent={sent} message={message.message} /> 
+                    <React.Fragment key={message.id}>
+                        <Message sent={sent} message={message.message} /> 
                         <MessagesReceivedRates time={'30 minutes'} price={29} rate={rate} sent={sent} />
                         <MessagesReceivedRates time={'1 hour'} price={49} rate={rate} sent={sent} />                    
-                    </>
+                    </React.Fragment>
                 ))}               
             </MessagesContainer>
         </>
