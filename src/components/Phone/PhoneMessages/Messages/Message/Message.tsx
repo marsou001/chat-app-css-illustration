fae3f0437b962ebed2ea742b3afa74a3da5fd365
Marouane Souda <marsou431@gmail.com>
2021-03-12 09:49:51 +0100

    refactor: change StyledProps to MessageProps and switch with IProps in Message.tsx

--- src/components/Phone/PhoneMessages/Messages/Message/Message.tsx
+++ src/components/Phone/PhoneMessages/Messages/Message/Message.tsx
@@ -1,27 +1,27 @@
 import styled from 'styled-components';
 
-interface StyledProps {
+interface IProps {
     sent: boolean
 }
 
-interface IProps extends StyledProps {
+interface MessageProps extends IProps {
     message: string
 }
 
 const MessageContainer = styled.div`
     font-size: 0.6rem;
-    color: ${(props: StyledProps) => props.sent ? 'hsl(271, 15%, 43%)' : 'hsl(276, 55%, 52%)'};
-    background-color: ${(props: StyledProps) => props.sent ? 'hsl(0, 0%, 100%)' : 'hsl(272, 41%, 93%)'};
+    color: ${(props: IProps) => props.sent ? 'hsl(271, 15%, 43%)' : 'hsl(276, 55%, 52%)'};
+    background-color: ${(props: IProps) => props.sent ? 'hsl(0, 0%, 100%)' : 'hsl(272, 41%, 93%)'};
     word-break: break-all;
     width: fit-content;
     max-width: 50%;
     margin-bottom: 8px;
     padding: 10px;
     border-radius: 10px;
-    border-bottom-${(props: StyledProps) => props.sent ? 'right' : 'left'}-radius: 5px;
+    border-bottom-${(props: IProps) => props.sent ? 'right' : 'left'}-radius: 5px;
 `;
 
-function Message({ sent, message }: IProps) {
+function Message({ sent, message }: MessageProps) {
     return (
         <MessageContainer sent={sent}>{message}</MessageContainer>
     )
